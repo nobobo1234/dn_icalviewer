bb59c75a0a0c5908753d20310f3a1c6da9abde38
Jake Jongejans <jakejongejans@gmail.com>
2021-08-31 20:51:53 +0200

    chore(json-parser): Start of parse stuff

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,12 @@ import TimeTable from './components/TimeTable';
 
 const App = () => {
     const [studentId, setStudentId] = useState('');
-    const [calendar, setCalendar] = useState<CalendarComponent[]>([]);
-
-    const getICal = async () => {
-        const events = await ical.async.fromURL('https://noahvb.nl/213222.ics');
-
-        setCalendar(Object.values(events));
-    };
 
     return (
         <div className="App">
             <input value={studentId} onChange={(e) => setStudentId(e.target.value)} />
-            <button type="button" onClick={getICal}>Verkrijg rooster</button>
 
-            <TimeTable calendar={calendar} />
+            <TimeTable studentId={Number.parseInt(studentId, 10)} week={36} year={2021} />
         </div>
     );
 };
--- src/components/TimeTable/index.tsx
+++ src/components/TimeTable/index.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
-import { CalendarComponent } from 'node-ical';
+import { useState } from 'react'
+import ical, { CalendarComponent } from 'node-ical';
+import PropTypes from 'prop-types'
 
-import Wrapper from './wrapper';
-
-type Props = {
-  /** Een lijst met alle events die uit een .ics-file geparset zijn. */
-  calendar: CalendarComponent[],
-};
+import Wrapper from './wrapper'
 
 /**
  * Dit is de hoofdcomponent van de timetable.
  * Hierin wordt de `calendar` variabele geïnterpreteerd en worden vervolgens alle rijen geprint.
  */
 /* eslint-disable no-unused-vars */
-const TimeTable = ({ calendar }: Props) => (
+const TimeTable = ({ studentId, week, year }) => {
+  const [calendar, setCalendar] = useState<CalendarComponent[]>([])
+
+  const getICal = async () => {
+    const events = await ical.async.fromURL('https://noahvb.nl/213222.ics');
+
+    setCalendar(Object.values(events));
+  };
+
+  console.log(calendar)
+
+  return (
     <Wrapper>
-        <h2>Test</h2>
+      <h2>Test</h2>
     </Wrapper>
-);
+  )
+}
+
+TimeTable.propTypes = {
+  studentId: PropTypes.number.isRequired,
+  week: PropTypes.number,
+  year: PropTypes.number,
+}
+
+TimeTable.defaultProps = {
+  week: 36,
+  year: 2021,
+}
 
 export default TimeTable;
